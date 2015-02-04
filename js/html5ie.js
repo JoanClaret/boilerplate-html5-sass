acf09800bf7efd5f0e7bb56b7b42bb4e8bc89363
Joan Claret <joan@androidzoom.com>
2015-02-04 12:18:58 +0100

    Adding IE support to media queries and html5 tags

---
+++ js/html5ie.js
@@ -0,0 +1,4 @@
+ var elements = ['section', 'article', 'aside', 'header', 'footer', 'nav', 'figure', 'figcaption', 'time', 'mark'];
+for( var i = 0; i < elements.length; i++ ) {
+    document.createElement(elements[i]);
+}
